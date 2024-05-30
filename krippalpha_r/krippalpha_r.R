81b921b5f817c9389f1521cf44d5668e52c1ba0f
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-05-30 07:35:41 +0700

    Use krippalpha (kripp.alpha) With (In) R Software

--- krippalpha_r/krippalpha_r.R
+++ krippalpha_r/krippalpha_r.R
@@ -1,18 +1,18 @@
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
-# Compute Krippendorff‘s intercoder reliability coefficient Alpha Use krippalpha (kripp.alpha) With (In) R Software
+# Compute Krippendorffâ€˜s intercoder reliability coefficient Alpha Use krippalpha (kripp.alpha) With (In) R Software
 install.packages("irr")
 install.packages("icr")
 library("irr")
 library("icr")
 krippalpha = read.csv("https://raw.githubusercontent.com/timbulwidodostp/krippalpha_r/main/krippalpha_r/krippalpha_r.csv",sep = ";")
-# Estimate Econometric Models for Spatial Panel Data Use spml With (In) R Software
+# Compute Krippendorffâ€˜s intercoder reliability coefficient Alpha Use krippalpha (kripp.alpha) With (In) R Software
 krippalpha <- as.matrix(krippalpha)
 kripp.alpha(krippalpha,"nominal")
 kripp.alpha(krippalpha, method = "nominal")
 krippalpha(krippalpha)
-# Compute Krippendorff‘s intercoder reliability coefficient Alpha Use krippalpha (kripp.alpha) With (In) R Software
+# Compute Krippendorffâ€˜s intercoder reliability coefficient Alpha Use krippalpha (kripp.alpha) With (In) R Software
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
